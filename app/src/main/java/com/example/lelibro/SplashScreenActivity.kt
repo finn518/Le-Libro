bde53499eaa20b7250f88f97aee44d2cb68bcc9d
Muhammad Helga <muhammadhelga41@gmail.com>
2024-10-23 19:38:54 +0700

    new update

--- app/src/main/java/com/example/lelibro/SplashScreenActivity.kt
+++ app/src/main/java/com/example/lelibro/SplashScreenActivity.kt
@@ -1,6 +1,8 @@
 package com.example.lelibro
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +11,19 @@ import androidx.core.view.WindowInsetsCompat
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_splash_screen)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        val decorView = window.decorView
+        // Hide the status bar.
+        val uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN
+        decorView.systemUiVisibility = uiOptions
+
+        // Hide ActionBar
+        supportActionBar?.hide()
+
+        // Timer
+        android.os.Handler().postDelayed({
+            startActivity(Intent(this@SplashScreenActivity, intro_page::class.java))
+            finish()
+        }, 3000)
     }
 }
