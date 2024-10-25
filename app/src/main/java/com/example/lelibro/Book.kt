89f7dface7a48389a8fbe567dec392f410e33d5a
Muhammad Helga <muhammadhelga41@gmail.com>
2024-10-25 17:31:21 +0700

    new up

---
+++ app/src/main/java/com/example/lelibro/Book.kt
@@ -0,0 +1,9 @@
+package com.example.lelibro
+
+data class Book (
+    var id: String? = null,
+    var judul: String? = null,
+    var penulis: String? = null,
+    var deskripsi: String? = null,
+    var genre: String? = null,
+)
---
+++ app/src/main/java/com/example/lelibro/BookAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.lelibro
+
+class BookAdapter {
+}
