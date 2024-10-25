f3884f3da590b30bd190f6920627c5733320a5da
Muhammad Helga <muhammadhelga41@gmail.com>
2024-10-25 17:33:46 +0700

    src

--- app/src/main/java/com/example/lelibro/AddBook.kt
+++ app/src/main/java/com/example/lelibro/AddBook.kt
@@ -1,16 +1,38 @@
 package com.example.lelibro
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.ScrollView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class AddBook : AppCompatActivity() {
+    private lateinit var popup_add: ScrollView
+    private lateinit var back_popup: ImageButton
+    private lateinit var add_cover: ImageView
+    private lateinit var addJudul: EditText
+    private lateinit var addPenulis: EditText
+    private lateinit var addDesk: EditText
+    private lateinit var addGenre: EditText
+    private lateinit var upItem: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_add_book)
+
+        popup_add = findViewById(R.id.popup_add)
+        back_popup = findViewById(R.id.back_popup)
+        add_cover = findViewById(R.id.upload_cover)
+        addJudul = findViewById(R.id.add_judul)
+        addPenulis = findViewById(R.id.add_penulis)
+        addDesk = findViewById(R.id.add_desk)
+        addGenre = findViewById(R.id.add_genre)
+        upItem = findViewById(R.id.btn_upload)
 
     }
 }
--- app/src/main/java/com/example/lelibro/MainActivity.kt
+++ app/src/main/java/com/example/lelibro/MainActivity.kt
@@ -20,29 +20,13 @@ class MainActivity : AppCompatActivity() {
     private lateinit var btnAdd: Button
     private lateinit var rv: RecyclerView
     private lateinit var dark_layout: ImageView
-    private lateinit var popup_add: ScrollView
-    private lateinit var back_popup: ImageButton
-    private lateinit var add_cover: ImageView
-    private lateinit var addJudul: EditText
-    private lateinit var addPenulis: EditText
-    private lateinit var addDesk: EditText
-    private lateinit var addGenre: EditText
-    private lateinit var upItem: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         btnAdd = findViewById(R.id.btn_add)
         rv = findViewById(R.id.rv)
-        dark_layout = findViewById(R.id.dark)
-        popup_add = findViewById(R.id.popup_add)
-        back_popup = findViewById(R.id.back_popup)
-        add_cover = findViewById(R.id.upload_cover)
-        addJudul = findViewById(R.id.add_judul)
-        addPenulis = findViewById(R.id.add_penulis)
-        addDesk = findViewById(R.id.add_desk)
-        addGenre = findViewById(R.id.add_genre)
-        upItem = findViewById(R.id.btn_upload)
 
         logoutBtn = findViewById(R.id.logout_btn)
         mAuth = FirebaseAuth.getInstance()
